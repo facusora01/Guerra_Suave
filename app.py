795a49f6aa01242bad6a5891567493ec62addf77
Facusora01 <fsorasio@fi.uba.ar>
2024-05-31 20:43:33 -0300

    Codigo Base adaptado a Flask

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+#activar debugger
+os.environ['FLASK_DEBUG'] = '1'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/rooms')
+def rooms():
+    return render_template('rooms.html')
+
+@app.route('/restaurant')
+def restaurant():
+    return render_template('restaurant.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/rooms_single')
+def rooms_single():
+    return render_template('rooms_single.html')
+
+@app.route('/blog_single')
+def blog_single():
+    return render_template('blog_single.html')
