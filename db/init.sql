454bb0507147ddf44b4c205759033bc2be5a77dc
CaroRuizDiaz <cruiz@fi.uba.ar>
2024-06-02 17:27:31 -0300

    Se han agregado las cabañas a la tabla "posadas" [6]

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS posadas (
     identificador SMALLINT UNIQUE,
     precioNoche DECIMAL(10,2),
-    metrosCuadrados TINYINT,
+    metrosCuadrados SMALLINT,
     ambientes TINYINT,
     camasUnicas TINYINT,
     camasMatrimoniales TINYINT,
@@ -13,6 +13,8 @@ CREATE TABLE IF NOT EXISTS posadas (
     foto1 VARCHAR(200),
     foto2 VARCHAR(200),
     foto3 VARCHAR(200),
+    foto4 VARCHAR(200),
+    foto5 VARCHAR(200),
     PRIMARY KEY (identificador)
 );
 
@@ -37,18 +39,115 @@ CREATE TABLE IF NOT EXISTS usuarios (
 INSERT INTO usuarios VALUES (UUID(), "Pedro", "Lopez", "p.lopez@gmail.com", "Pedropass11");
 
 INSERT INTO posadas VALUES (
-    103,  
-    3500.00,
-    58,
-    4,
-    1,
-    2,
-    1,
-    1,
-    0,
-    "lago",
-    "Hermosa postada con vista al lago, espaciosa y equipada para una agradables vacaciones",
-    "https://kabania.ca/wp-content/uploads/2019/03/cabanes-pilotis-pilotis.jpg",
-    "",
-    ""
+	101,
+	60000.00,
+	70,
+	3,
+	2,
+	1,
+	1,
+	1,
+	0,
+	"complejo",
+	" -Heladera -Microondas -Termotanque -Cocina a gas -Cafetera -Calefactor -Aire acondicionado frio-calor - Secador de pelo - Smart TV",
+	"static/images/Amapola/amapola1.jpg",
+	"static/images/Amapola/amapola2.jpg",
+	"static/images/Amapola/amapola3.jpg",
+	"static/images/Amapola/amapola4.jpg",
+	"static/images/Amapola/amapola5.jpg"
+);
+
+INSERT INTO posadas VALUES (
+	201,
+	80000.00,
+	100,
+	3,
+	3,
+	1,
+	1,
+	1,
+	1,
+	"complejo",
+	"-Hogar a lenia -Aire acondicionado -Baniera de hidromasaje -TV -Cafetera -Heladera  -Cafetera -Microondas -Secador de pelo -Horno - Tostadora",
+	"static/images/TruchaDorada/truchaDorada1.jpg",
+	"static/images/TruchaDorada/truchaDorada2.jpg",
+	"static/images/TruchaDorada/truchaDorada3.jpg",
+	"static/images/TruchaDorada/truchaDorada4.jpg",
+	"static/images/TruchaDorada/truchaDorada5.jpg"
+);
+
+INSERT INTO posadas VALUES (
+	301,
+	200000.00,
+	120,
+	5,
+	5,
+	1,
+	1,
+	1,
+	0,
+	"lago",
+	" -Banio en suite -Cocina completa -Smart TV -Hogar a lenia -Banios completos",
+	"static/images/CiervoBlanco/ciervoBlanco1.jpg",
+	"static/images/CiervoBlanco/ciervoBlanco2.jpg",
+	"static/images/CiervoBlanco/ciervoBlanco3.jpg",
+	"static/images/CiervoBlanco/ciervoBlanco4.jpg",
+	"static/images/CiervoBlanco/ciervoBlanco5.jpg"
+);
+
+INSERT INTO posadas VALUES (
+	401,
+	40000.00,
+	40,
+	2,
+	0,
+	1,
+	1,
+	1,
+	1,
+	"bosque",
+	"- Cocina completa - Lavadora -Banio completo -Smart TV  -Secador de pelo -Secadora de ropa ",
+	"static/images/HierbaAlta/hierbaAlta1.jpg",
+	"static/images/HierbaAlta/hierbaAlta2.jpg",
+	"static/images/HierbaAlta/hierbaAlta3.jpg",
+	"static/images/HierbaAlta/hierbaAlta4.jpg",
+	"static/images/HierbaAlta/hierbaAlta5.jpg"
+);
+
+INSERT INTO posadas VALUES (
+	501,
+	250000.00,
+	150,
+	5,
+	6,
+	2,
+	1,
+	1,
+	0,
+	"bosque",
+	"-Heladera -Microondas -Termotanque -Cocina a gas -Cafetera -Calefactor - Aire acondicionado frio/calor -Secador de pelo -Smart TV",	
+	"static/images/BosqueAlto/bosqueAlto1.jpg","static/images/BosqueAlto/bosqueAlto2.jpg",
+	"static/images/BosqueAlto/bosqueAlto3.jpg",
+	"static/images/BosqueAlto/bosqueAlto4.jpg",
+	"static/images/BosqueAlto/bosqueAlto5.jpg"
+);
+
+
+INSERT INTO posadas VALUES (
+	601,
+	1250000.00,
+	200,
+	2,
+	0,
+	1,
+	1,
+	1,
+	1,
+	"bosque",
+	"-Hogar a lenia -Aire acondicionado - Baniera de hidromasaje -Smart TV -Cafetera -Heladera -Cafetera -Microondas -Secador de pelo - Horno -Tostadora",
+	"static/images/Carpincho/carpincho7.jpg",
+	"static/images/Carpincho/carpincho6.jpg",
+	"static/images/Carpincho/carpincho5.jpg",
+	"static/images/Carpincho/carpincho4.jpg",
+	"static/images/Carpincho/carpincho3.jpg"
 );
