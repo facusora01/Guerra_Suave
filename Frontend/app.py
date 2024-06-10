e065565ac7c5e11aef0052a5a7e7e67e6008db0e
Julen Gaumard <julengaumard@gmail.com>
2024-06-10 20:11:07 -0300

    Emprolijando pagina de posada, quitando elementos sin utilidad y destacando informacion importante [51]

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import os
-import json
 import requests
 
 app = Flask(__name__)
@@ -38,55 +37,11 @@ def blog():
 def contact():
     return render_template('contact.html')
 
-@app.route('/Amapola', methods=['GET'])
-def Amapola():
-    try:
-        response = requests.get('http://127.0.0.1:5050/posadas')
-        Lista_Cabanas = response.json()
-    except requests.exceptions.RequestException as error:
-        print("=====================================")
-        print(f"Error en la solicitud: {error}")
-        print("=====================================")
-        Lista_Cabanas = {'posadas': []}  # Inicializar con una lista vacía en caso de error
-    except ValueError as error:
-        print("=====================================")
-        print(f"Error al decodificar JSON: {error}")
-        print("=====================================")
-        Lista_Cabanas = {'posadas': []}  # Inicializar con una lista vacía en caso de error
-
-    posada = None
-    for cabana in Lista_Cabanas['posadas']:
-        if cabana['identificador'] == 201:  # Cambia el identificador por el que necesites
-            posada = cabana
-            break
-    
-    return render_template('posada.html', posada=posada)
-
-@app.route('/Carpincho')
-def Carpincho():
-    return render_template('Cabana_Carpincho.html')
-
-@app.route('/Ciervo_Blanco')
-def Ciervo_Blanco():
-    return render_template('Cabana_Ciervo_Blanco.html')
-
-@app.route('/Hierba_Alta')
-def Hierba_Alta():
-    return render_template('Cabana_Hierba_Alta.html')
-
-@app.route('/Bosque_Alto')
-def Bosque_Alto():
-    return render_template('Cabana_Bosque_Alto.html')
-
-@app.route('/Trucha_Dorada')
-def Trucha_Dorada():
-    return render_template('Cabana_Trucha_Dorada.html')
-
 @app.route('/blog_single')
 def blog_single():
     return render_template('blog_single.html')
 
-### Reservas
+
 @app.route('/reservar', methods=["POST"])
 def reservar():
 
@@ -104,11 +59,13 @@ def reservar():
     
     return redirect(url_for('obtenerPosada', id=data['identificadorPosada'], fechaIngreso=data['fechaIngreso'], fechaEgreso=data['fechaEgreso'], error=response.json()['message']))
 
+
 @app.route('/misreservas')
 def misReservas():
     response = requests.get('http://127.0.0.1:5050/reservas?email=p.lopez@gmail.com')
     response = response.json()
     return render_template('misreservas.html', reservas=response['reservas'], correcta=request.args.get('correcta'))
+
 
 @app.route('/cancelar', methods=['POST'])
 def cancelarReserva():
@@ -121,6 +78,7 @@ def cancelarReserva():
     requests.delete('http://127.0.0.1:5050/reservas', json=data)
     return redirect(url_for('misReservas'))
 
+
 @app.route('/posadas/<id>', methods=['GET'])
 def obtenerPosada(id):
     fechas = {
@@ -130,8 +88,10 @@ def obtenerPosada(id):
     response = requests.get(f"""http://127.0.0.1:5050/posadas?identificador={id}""")
     posada = response.json()['posadas'][0]
     posada['descripcion'] = posada['descripcion'].split("-")
+    posada['descripcion'] = filter(lambda x: x != "", posada['descripcion'])
   
     return render_template('posada.html', posada=posada, reservable=True, fechas=fechas, error=request.args.get("error"))
+
 
 @app.route('/buscar', methods=["POST"])
 def buscarPosadas():
@@ -148,17 +108,6 @@ def Cabanas():
     cantidad = data['cantidad']
     return render_template('Cabanas.html', cabanas=posadas, cantidad=cantidad, filtros={})
 
-
-@app.route("/conexion_a_reservas", methods=["GET"])
-def conexion_a_reservas():
-    response = requests.get('http://127.0.0.1:5050/reservas')
-    data = response.json()
-
-    reservas = data['reservas']
-    cantidad = data['cantidad']
-
-    return render_template("cone_a_rese.html", reservas=reservas, cantidad=cantidad)
-    #return jsonify(data)
 
 @app.errorhandler(404)
 def error(e):
